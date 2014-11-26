c9ba8812f4f852bea594283a04e8cb3e3c093c72
Shelly M. Poole <Swimmingrocks@gmail.com>
2014-11-26 12:46:35 -0600

    ugh price is a int change it

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,4 +1,17 @@
 ActiveAdmin.register Product do
 
-  permit_params :name, :quantity, :price, :picture, :description
+  permit_params :name, :quantity, :price, :picture, :description, :category_id
+
+  form :html => {:multipart => true} do |f|
+  f.inputs "Product Details" do
+    f.input :name
+    f.input :description
+    f.input :price
+    f.input :quantity
+    f.input :category
+    f.input :picture
+  end
+  f.actions
+end
+
 end
